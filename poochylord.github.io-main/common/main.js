af96b97398951fadad8a28c6f4e888e0fad54966
Poochy <111499482+Poochylord@users.noreply.github.com>
2026-05-29 16:06:10 +0200

    Delete poochylord.github.io-main directory

--- poochylord.github.io-main/common/main.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementsByClassName("button-submit")
-const inputthing = document.getElementById("info")
-const tx = document.getElementById("hi")
-
-function yes(){
-  tx.innerHTML = inputthing.value
-}
-
-btn.addEventListener("click", yes);
