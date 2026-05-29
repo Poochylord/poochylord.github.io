1c2ee997fbb4ed309b2c5fb22ecc5653f1e31ae7
Poochy <111499482+Poochylord@users.noreply.github.com>
2026-05-29 16:06:51 +0200

    Delete scr directory

--- scr/main.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById("submit")
-const inputthing = document.getElementById("title")
-const tx = document.getElementById("hi")
-
-function yes(){
-  tx.innerHTML = inputthing.value
-}
-
-btn.addEventListener("click", yes);
